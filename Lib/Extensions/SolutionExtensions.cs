eb91a0574407fe7317c6a70e5f21515f50b31127
pirren <pierre.nygard@gmail.com>
2021-05-31 22:38:50 +0200

    extensions

--- Lib/Extensions/SolutionExtensions.cs
+++ Lib/Extensions/SolutionExtensions.cs
@@ -11,68 +11,30 @@ namespace Aoc.Lib.Extensions
     public static class SolutionExtensions
     {
         /// <summary>
-        /// Prints both problems
+        /// Gets both solutions
         /// </summary>
         /// <param name="solver"></param>
         /// <returns>Result</returns>
-        public static Result SolveBoth(this ISolver solver)
+        public static Result<object[]> GetSolutions(this ISolver solver)
         {
-            try
-            {
-                IEnumerable<object> solutions = solver.Solve();
-                PrintStartMessage(solver);
-                SystemUtils.Print($"Part 1: {solutions.First() ?? 0}\n");
-                SystemUtils.Print($"Part 2: {solutions.Last() ?? 0}\n");
-                PrintFinishedMessage();
-                return Result.Ok();
-            }
-            catch
-            {
-                return Result.Fail("Caught error trying to run sulutions");
-            }
+            return Result.Ok(solver.Solve().ToArray());
         }
 
         /// <summary>
-        /// Prints a given problem 
+        /// Gets a single solution
         /// </summary>
         /// <param name="solver"></param>
         /// <param name="choice"></param>
         /// <returns>Result</returns>
-        public static Result SolveSingle(this ISolver solver, Solution choice)
+        public static Result<object> GetSolution(this ISolver solver, Solution choice)
         {
-            try
-            {
-                IEnumerable<object> solutions = solver.Solve();
-                PrintStartMessage(solver);
-                SystemUtils.Print(new StringBuilder()
-                        .Append((string)(choice == Solution.One ? solutions.First() : solutions.Last()))
-                        .Append('\n')
-                        .ToString()
-                    );
-                PrintFinishedMessage();
-                return Result.Ok();
-            }
-            catch
-            {
-                return Result.Fail("Caught error trying to run sulutions");
-            }
+            IEnumerable<object> solutions = solver.Solve();
+            return Result.Ok(choice == Solution.One ? solutions.First() : solutions.Last());
         }
 
-        private static void PrintStartMessage(ISolver solver)
+        public static Problem GetProblemInfo(this ISolver solver)
         {
-            Problem problemInfo = (Problem)System.Attribute.GetCustomAttributes(solver.GetType()).First();
-
-            SystemUtils.Print("\n========================================\n");
-            SystemUtils.Print("Problem name: ");
-            SystemUtils.Print(new StringBuilder().Append(problemInfo.GetName()).Append('\n').ToString(), ConsoleColor.Red); 
-            SystemUtils.Print("Day: ");
-            SystemUtils.Print(new StringBuilder().Append(problemInfo.GetDay()).Append("\n\n").ToString(), ConsoleColor.DarkRed); // fix this with Attribute reflection
-        }
-
-        private static void PrintFinishedMessage()
-        {
-            SystemUtils.Print("\nFinished");
-            SystemUtils.Print("\n========================================\n");
+            return (Problem)System.Attribute.GetCustomAttributes(solver.GetType()).First();
         }
     }
 }
